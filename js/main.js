fca19e3e456a03f8c3ee32914763a4b97280a39a
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-04-22 11:34:48 -0400

    content for second slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".fairy-tale__slider, .our-trip__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg"/></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg"/></button>',
+    autoplay: true,
+    fade: true,
+  });
+});
